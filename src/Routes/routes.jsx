efa0c1a7c10d939f935f0d6e36719350d6c4e73b
MimFSD <mimislam878@gmail.com>
2024-08-24 00:10:11 +0600

    added tittle & logp pic

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,11 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+  import "./index.css";
+  
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
