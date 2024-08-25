70af0a2f4dda4a40622b8c4e30bd6df27d47507f
MimFSD <mimislam878@gmail.com>
2024-08-26 00:31:41 +0600

    Login & Quarries pages added

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,109 @@
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
+
+
+const Login = () => {
+    const { login, googleSignIn, githubSignIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const pass = form.pass.value;
+        login(email, pass).then((userCredential) => {
+            const user = userCredential.user;
+            navigate(location?.state ? location?.state : "/")
+        })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage)
+            });
+    }
+
+    const handleGoogleLogin = () => {
+        googleSignIn()
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+                navigate(location?.state ? location?.state : "/")
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+    // const handleGithubLogin = () => {
+    //     githubSignIn()
+    //         .then((result) => {
+    //             // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    //             const credential = GithubAuthProvider.credentialFromResult(result);
+    //             const token = credential.accessToken;
+
+    //             // The signed-in user info.
+    //             const user = result.user;
+    //             // IdP data available using getAdditionalUserInfo(result)
+    //             // ...
+    //             navigate(location?.state ? location?.state : "/")
+
+    //         }).catch((error) => {
+    //             // Handle Errors here.
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             // The email of the user's account used.
+    //             const email = error.customData.email;
+    //             // The AuthCredential type that was used.
+    //             const credential = GithubAuthProvider.credentialFromError(error);
+    //             // ...
+    //         });
+
+    // }
+    return (
+        <div className="max-w-7xl mx-auto flex justify-center mb-8">
+            <div className="card shrink-0 w-full max-w-sm py-10 shadow-2xl bg-base-100">
+                <form className="card-body" onSubmit={handleLogin}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="pass" placeholder="password" className="input input-bordered" required />
+                        {/* <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label> */}
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+                <div className="flex text-3xl gap-4 justify-center ">
+                    <FaGoogle onClick={handleGoogleLogin} className="hover:cursor-pointer" />
+                    {/* <FaGithub onClick={handleGithubLogin} className="hover:cursor-pointer" /> */}
+
+                </div>
+                <p className="text-center">If you don't have an account then <Link to="/signup" className="text-blue-500">Register</Link> first.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
